73a9bbb961a456f4be2a8d1f3d32fefdaee34fce
bhagya0705 <edak.bhagya@gmail.com>
2025-07-09 13:53:22 +0530

    fixed crashing issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,10 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
 import userRoutes from './routes/user.routes.js';
 
 app.use('/api/v1/users',userRoutes);
